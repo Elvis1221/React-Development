046e76f4da4d661ab82b223ec79a834ad7271326
Andrei <fujitsuelvis@gmail.com>
2020-12-20 07:23:59 +0300

    Lesson 14

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from './Header.module.css'
+
+const Header = () => {
+    return <header className={s.header}>
+        <img
+            src="https://pngimg.com/uploads/intel/intel_PNG11.png"
+            alt=""/>
+    </header>
+};
+export default Header
